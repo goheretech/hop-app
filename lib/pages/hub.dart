15d57c1b23ab83313ea1c2ac09b5e0e065cf4deb
goheretech <clemay@gohere.tech>
2021-11-03 11:33:38 -0500

    wow

--- lib/pages/hub.dart
+++ lib/pages/hub.dart
@@ -43,7 +43,7 @@ class _HubState extends State<Hub> {
                       0.6,
                       "img/hop3.png",
                       Icons.gavel_outlined,
-                      'https://app.galabid.com/2021-wishes-gala/',
+                      'https://app.galabid.com/2021-wishes-gala/categories',
                     ),
                   ),
                   Expanded(
